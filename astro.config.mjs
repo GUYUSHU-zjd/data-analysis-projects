07000fc0719df27665153db146cf046c748b5a59
GUYUSHU-zjd <3255768319@qq.com>
2025-09-13 23:56:49 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// 根据环境变量动态设置 base 路径
+// 根据环境动态设置 base 路径
 const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 const base = isGitHubPages ? '/data-analysis-projects/' : '/';
 
 export default defineConfig({
   integrations: [tailwind()],
-  base: base, // 动态设置 base 路径
+  base: base,
   site: 'https://guyushu-zjd.github.io',
   output: 'static',
   trailingSlash: 'always',
   outDir: 'dist',
   vite: {
     build: {
-      cssCodeSplit: false, // 禁用 CSS 代码分割，解决预加载问题
+      cssCodeSplit: false, // 禁用 CSS 代码分割
     },
   },
 });
